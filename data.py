131bab74d1e22f5f580303e06b79688bb9c05c29
Hippolyte <hippolyte.heuberger@gmail.com>
2017-11-08 10:58:20 +0000

    take account of the number of epochs

--- data.py
+++ data.py
@@ -12,7 +12,6 @@ import skimage.transform
 import random
 import json
 
-
 class Batcher():
 
     def __init__(self, data_path, config, vocab):
@@ -28,13 +27,17 @@ class Batcher():
         self.im_height = config['im_height']
         self.max_len = config['nb_LSTM_cells']
         self.vocab = vocab
-
+        self.current_idx = 0
         self.epoch_completed = 0
         
     def load_annotations(self):
         annot = json.load(open(self.train_path+'annotations/captions_train2014.json', 'r'))
         available_im = os.listdir(self.train_path+'images/')
         self.ids = [elem['id'] for elem in annot['images'] if elem['file_name'] in available_im]
+        
+        self.nb_ids = len(self.ids)
+        #random.shuffle(self.ids)
+        
         captions_list = [elem for elem in annot['annotations'] if elem['image_id'] in self.ids]
         self.captions = pd.DataFrame(captions_list).groupby('image_id')['caption'].apply(list)
 
@@ -59,21 +62,29 @@ class Batcher():
         :return: a batch containing images and their encoded annotations, picked in the paths folder. Deals with
         the number of completed epochs
         """
+        next_idx = self.current_idx+4
+        batch_ids = self.ids[self.current_idx:next_idx]
         
-        random.shuffle(self.ids)
-        batch_ids = self.ids[:4]
+        if next_idx > self.nb_ids:
+            self.epoch_completed +=1
+            random.shuffle(self.ids)
+            remaining = next_idx - self.nb_ids
+            batch_ids += self.ids[:remaining]
+            next_idx = remaining
+            
         imgs = np.zeros((self.batch_size, self.im_width, self.im_height, 3), dtype=np.float)
         labels = np.zeros((self.batch_size, 1), dtype=np.float)
         
         
         for image_id in batch_ids:
-            idx = image_id % self.batch_size
+            batch_idx = image_id % self.batch_size
+            print(image_id)
             img_name = 'COCO_train2014_000000' + str(image_id) + '.jpg'
-            imgs[idx,...] = self.load_image(self.train_path + 'images/' + img_name)
-            labels[idx,...] = self.make_labels(self.captions[image_id])
-            
-            if idx == self.batch_size - 1:
-                yield imgs, labels
+            imgs[batch_idx,...] = self.load_image(self.train_path + 'images/' + img_name)
+            labels[batch_idx,...] = self.make_labels(self.captions[image_id])
+        
+        self.current_idx = next_idx
+        return imgs, labels
         
     
     def make_labels(self, captions_list):
@@ -93,6 +104,7 @@ class Batcher():
 
     def encode_sentence(self, sentence, vocab):
         """
+
         :param sentence: Raw sentence to encode
         :param vocab: global vocab file.
         :return: Encoded sentence with IDs of the words, padded to a fixed size length.
