d60601231a21c43566a849e09686317c0ac5cce5
Wacim Belblidia <wacim@illuin.tech>
2017-11-08 15:19:31 +0100

    Correcting accidental deletes. Adding shape information.

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 # Defining the model
 import tensorflow as tf
 from typing import Dict, List, Tuple
+import numpy as np
 
 class CaptioningNetwork():
     def __init__(self, config):
@@ -44,29 +45,41 @@ class CaptioningNetwork():
         """
         :return: Creates, or returns, the graph corresponding to the model.
         """
+        # Create placeholders
+        self.create_placeholders()
+
         # Creating model
         self.add_model()
 
         # Creating the training operators (loss/optimizers etc)
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
         self.add_train_op()
+        self.summaries = tf.summary.merge_all()
 
-    def add_model(self, CNN_lastlayer, caption_embedding):
+    def add_model(self):
         """
         :return: Creates model (final variable from feed_dict). The output is to define the self.out_tensor
         object, which is the final prediction tensor used in the loss function
         """
+
+        CNN_lastlayer = tf.Variable(np.random.rand(self.hyps['batch_size'], self.hyps['hidden_dim']))
+
+        # self.y_pl has shape (batch_size, max_sentence_length)
+        # caption_embedding has shape (batch_size, max_sentence_length, embedding_size)
+        caption_embedding = self.embedding(self.y_pl)
+
         rand_unif_init = tf.random_uniform_initializer(-self.hyps.rand_unif_init_mag, self.hyps.rand_unif_init_mag,
                                                        seed=123)
-        # CNN_lastlayer : [batch_size, LSTM_dim]
-        # caption_embedding : [batch_size, nb_LSTM_cells, embedding_size] where embedding_size = LSTM_dim
+        # CNN_lastlayer : [batch_size, hidden_dim]
+        # caption_embedding : [batch_size, nb_LSTM_cells, embedding_size] where embedding_size = hidden_dim
 
 
-        lstmcell = tf.contrib.rnn.LSTMCell(self.hyps.LSTM_dim, initializer=rand_unif_init)
+        lstmcell = tf.contrib.rnn.LSTMCell(self.hyps.hidden_dim, initializer=rand_unif_init)
         outputs, state = tf.nn.dynamic_rnn(cell=lstmcell, initial_state=CNN_lastlayer, inputs=caption_embedding, dtype=tf.float32)
 
-        # outputs : [batchsize, nombre de mots, LSTM_dim]
+        # outputs : [batchsize, nombre de mots, hidden_dim]
 
-        W_out = tf.get_variable('W_out', [self.hyps.LSTM_dim, self.hyps.vocab_size])
+        W_out = tf.get_variable('W_out', [self.hyps.hidden_dim, self.hyps.vocab_size])
         b_out = tf.get_variable('b_out', [self.hyps.vocab_size])
         
         out_tensor = []
@@ -78,14 +91,14 @@ class CaptioningNetwork():
         vocab_dists = tf.stack(vocab_dists)
         self.out_tensor = vocab_dists
             
-    def calculate_loss(self, preds, y_out_pl):
+    def calculate_loss(self, preds, y_pl):
         """
         :param preds: Prediction of the forward pass, as vocabulary distributions. Shape (batch_size, max_sentence_length, vocab_size)
-        :param y_out_pl: True target. Shape (batch_size, sentence_length)
-        :return: Likelihood of the prediction (defined in the paper). The loss is an element of the graph (Tensor)
+        :param y_pl: True target. Shape (batch_size, sentence_length)
+        :return: Likelihood of the prediction (defined in the paper). The  is an element of the graph (Tensor)
         """
 
-        temp = tf.reshape(y_out_pl, [self.hyps['batch_size'], self.hyps['max_sentence_length'], 1])
+        temp = tf.reshape(y_pl, [self.hyps['batch_size'], self.hyps['max_sentence_length'], 1])
 
         unstacked_pred = tf.unstack(preds)
         unstacked_true = tf.unstack(temp)
@@ -101,16 +114,27 @@ class CaptioningNetwork():
         """
         Assigns to a class variable the training operator (gradient iteration)
         """
-        self.loss = self.calculate_loss(self.out_tensor, self.y_out_pl)
+        self.loss = self.calculate_loss(self.out_tensor, self.y_pl)
         optimizer = tf.train.GradientDescentOptimizer(self.hyps['learning_rate'])
         grads_and_vars = optimizer.compute_gradients(self.loss)
         self.train_op = optimizer.apply_gradients(grads_and_vars, global_step=self.global_step)
 
-    def run_train_step(self, batch: Tuple):
+    def run_train_step(self, sess, batch: Tuple):
         """
         :param batch: Batch of images and annotations
         :return: run with a TF session a batch iteration, defining feed dict, fetches, etc.
         """
+
+        feed_dict = self.make_feed_dict(batch)
+        fetches = {'loss': self.loss,
+                   'train_op': self.train_op,
+                   'summary': self.summaries,
+                   'global_step': self.global_step
+                   }
+
+        result = sess.run(fetches=fetches, feed_dict=feed_dict)
+
+        return result
 
     def predict(self, input):
         """
@@ -118,7 +142,12 @@ class CaptioningNetwork():
         :param input: image to feed-forward
         :return: The generated caption
         """
+        ##### WARNING - The graph has to be updated to be working with the inference mode (No target in the feed_dict)
 
-        # Feed forward with the trained model
-        generated_caption = ...
+        fetches = [self.out_tensor]
+        feed_dict = {self.X_pl: input}
+
+        with tf.Session() as sess:
+            generated_caption = sess.run(fetches, feed_dict)
+
         return generated_caption
